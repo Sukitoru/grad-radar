00371fb32b1fc48995b23924680a3c4313b29d56
sukitoru <foreign.trill5@gmail.com>
2026-08-04 10:22:24 -0400

    Create Ionic React app

---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gradradar.app',
+  appName: 'Grad Radar',
+  webDir: 'dist'
+};
+
+export default config;
