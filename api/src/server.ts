cc242f54bd9cb9227f7264c5b5c53c1ab1bd375a
sukitoru <foreign.trill5@gmail.com>
2026-08-06 11:55:21 -0400

    Add Express server

---
+++ api/src/server.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+
+import app from './app.ts';
+
+const port = Number(process.env.PORT) || 3000;
+
+app.listen(port, () => {
+  console.log(`Grad Radar API is running on port ${port}`);
+});
