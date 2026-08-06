e4bbc394731601ff2d59b3970a5ea5be6a9650e2
samnramos <samantha.ramos793@login.cuny.edu>
2026-08-06 13:09:03 -0400

    added the read application and delete entries

--- api/src/routes/applications.ts
+++ api/src/routes/applications.ts
@@ -53,4 +53,74 @@ router.post('/applications', async (req, res) => {
   }
 });
 
+/* List all applications */
+
+router.get("/applications", async (req, res) => {
+    try {
+        const applications = await prisma.application.findMany();
+
+        res.status(200).json(applications);
+    } catch (error) {
+        res.status(500).json ({
+            message: "Failed to retrieve applications.",
+        }); 
+    }
+}); 
+
+/* Get's one application by ID */
+
+router.get("/applications/:id", async (req, res) => {
+    try {
+        const application = await prisma.application.findUnique ({
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!application) {
+            return res.status(404).json ({
+                error: "Application not found", 
+            });
+        }
+
+        res.status(200).json(application);
+    } catch (error) {
+        res.status(500).json({
+            message: "Failed to retrieve application.",
+        });
+    }
+});
+
+/* Deletes's an application*/
+
+router.delete("/applications/:id", async (req, res) => {
+    try {
+        const application = await prisma.application.findUnique ({
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!application) {
+            return res.status(404).json({ 
+                message: "Application not found.",
+            });
+        }
+
+        await prisma.application.delete ({
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        res.status(200).json ({
+            message: "Application deleted successfully",
+        });
+    } catch (error) {
+        res.status(500).json ({
+            message: "Failed to delete application.",
+        });
+    }
+});
+
 export default router;
